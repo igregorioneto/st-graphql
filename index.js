acd9d18fd766e8072f3edb4f2689e358a8e3c848
igregorioneto <igregoriodoneto@outlook.com>
2021-10-15 10:02:17 -0300

    criando mutations e terminando o CRUD e refatorando métodos

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { ApolloServer } = require("apollo-server");
+const { formatError } = require("graphql");
 const graphql = require("./src/graphql");
 
 const server = new ApolloServer(
   ...graphql,
+  formatError: (err) => {
+    if (err.message.startsWith("Usuário Existente:")) {
+      return new Error(err.message);
+    }
+
+    return err;
+  },
 );
 
 server.listen().then(({ url }) => console.log(url));
--- src/graphql/models/Usuario/usuario.resolvers.js
+++ src/graphql/models/Usuario/usuario.resolvers.js
@@ -1,5 +1,17 @@
 const db = require("../../../db");
 
+function geradorDeId(lista) {
+  let novoId;
+  let ultimo = lista[lista.length - 1];
+  if (ultimo) {
+    novoId = 0;
+  } else {
+    novoId = ultimo.id;
+  }
+
+  return ++novoId;
+}
+
 module.exports = {
   Usuario: {
     perfil(usuario) {
@@ -7,9 +19,68 @@ module.exports = {
     },
   },
   Query: {
-    usuario(obj, args) {
-      return db.usuarios.find((db) => db.id === args.id);
+    usuario(_, { filtro: { id, email } }) {
+      return buscarUsuario(id ? { id } : { email });
     },
     usuarios: () => db.usuarios,
+  },
+  Mutation: {
+    criarUsuario(_, { data }) {
+      const { email } = data;
+
+      const usuarioExistente = db.usuarios.some((u) => u.email === email);
+
+      if (usuarioExistente) {
+        throw new Error(`Usuário Existente: ${data.nome}`);
+      }
+
+      const novoUsuario = {
+        ...data,
+        id: geradorDeId(db.usuarios),
+        perfil_id: 2,
+      };
+
+      db.usuarios.push(novoUsuario);
+
+      return novoUsuario;
+    },
+    atualizarUsuario(_, { id, data }) {
+      const usuario = db.usuarios.find((u) => u.id === id);
+      const indice = db.usuarios.findIndex((u) => u.id === id);
+
+      const novoUsuario = {
+        ...usuario,
+        ...data,
+      };
+
+      db.usuarios.splice(indice, 1, novoUsuario);
+
+      return novoUsuario;
+    },
+    deletarUsuario(_, { filtro: { id, email } }) {
+      return deletarUsuarioFiltro(id ? { id } : { email });
+    },
   }
+}
+
+function deletarUsuarioFiltro(filtro) {
+  const chave = Object.keys(filtro)[0];
+  const valor = Object.values(filtro)[0];
+
+  const usuarioEncontrado = db.usuarios.find((u) => u[chave] === valor);
+  db.usuarios = db.usuarios.filter((u) => u[chave] !== valor);
+
+  return !!usuarioEncontrado;
+}
+
+function buscarUsuario(filtro) {
+  const chave = Object.keys(filtro)[0];
+  const valor = Object.values(filtro)[0];
+
+  return db.usuarios.find((db) => db[chave] === [valor]);
+  // if (filtro.id) {
+  //   return db.usuarios.find((db) => db.id === filtro.id);
+  // } else {
+  //   return db.usuarios.find((db) => db.email === filtro.email);
+  // }
 }
